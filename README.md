f5f6c33d84999c85a399e3deef3e1d58e88a6e8f
Mihai Fufezan <fufexan@protonmail.com>
2022-09-27 14:04:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">fufexan/infra</h1>
+
+# 🗒 About
+
+Declarative server management using Terraform.
---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    pkgs = nixpkgs.legacyPackages;
+    genSystems = nixpkgs.lib.genAttrs ["aarch64-linux" "x86_64-linux"];
+  in {
+    devShells = genSystems (system: {
+      default = pkgs.${system}.mkShell {
+        buildInputs = with pkgs.${system}; [
+          oci-cli
+          sops
+          terraform
+        ];
+      };
+    });
+  };
+}
