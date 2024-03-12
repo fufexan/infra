4453b8f698bcad4f8938f21b69079d9764d5cc41
Mihai Fufezan <fufexan@protonmail.com>
2024-03-13 00:29:46 +0200

    secrets: rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   mihai = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOq9Gew1rgfdIyuriJ/Ne0B8FE1s8O/U2ajErVQLUDu9 mihai@io";
 
-  alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAq1Bv6xXbjFI9bZoVJWgjfLQ6Pe82OWPhA/wVESQ5Tm fufexan.net";
+  alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAq1Bv6xXbjFI9bZoVJWgjfLQ6Pe82OWPhA/wVESQ5Tm root@alpha";
 in {
   "vaultwarden-env.age".publicKeys = [mihai alpha];
 }
