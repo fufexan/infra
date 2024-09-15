3931f40e13a1e52879e65654458b22ce095687b3
Mihai Fufezan <mihai@fufexan.net>
2024-09-15 22:14:43 +0300

    hosts/germanium: reflect actual config

--- hosts/germanium/default.nix
+++ hosts/germanium/default.nix
@@ -1,11 +1,27 @@
 {
   imports = [
-    ./hardware.nix
-    ./impermanence.nix
+    ./hardware-configuration.nix
     # ./minecraft.nix
   ];
 
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+  };
+
+  systemd.network = {
+    networks.ethernet.extraConfig = ''
+      [Match]
+      Type = ether
+      [Network]
+      Address = 2a01:4f8:c013:eb9::1/64
+      DHCP = ipv4
+      IPv6AcceptRA = no
+      LinkLocalAddressing = no
+    '';
+  };
+
   networking.hostName = "germanium";
 
-  services.getty.autologinUser = "root";
+  system.stateVersion = "24.05";
 }
