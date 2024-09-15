a550e9680354e40573a136ca79752abe14a26c43
Mihai Fufezan <mihai@fufexan.net>
2024-09-16 01:21:32 +0300

    modules/website: fix + add dotfiles redirect

--- modules/website.nix
+++ modules/website.nix
@@ -1,5 +1,11 @@
 {
   services.caddy.virtualHosts."fufexan.net".extraConfig = ''
+    encode zstd gzip
     root * /var/www/fufexan.net
+    file_server
+  '';
+
+  services.caddy.virtualHosts."dots.fufexan.net".extraConfig = ''
+    redir https://github.com/fufexan/dotfiles
   '';
 }
