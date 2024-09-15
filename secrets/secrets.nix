843061cad338ce09f236a0d147a19e81792a69f9
Mihai Fufezan <mihai@fufexan.net>
2024-09-15 22:19:21 +0300

    secrets: rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   mihai = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOq9Gew1rgfdIyuriJ/Ne0B8FE1s8O/U2ajErVQLUDu9 mihai@io";
 
-  alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAq1Bv6xXbjFI9bZoVJWgjfLQ6Pe82OWPhA/wVESQ5Tm root@alpha";
+  germanium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH59Ji3FQo4DkTW9knLuXpX/BC7IEEfiO65CJdT7Dw2l root@germanium";
 in {
-  "vaultwarden-env.age".publicKeys = [mihai alpha];
+  "vaultwarden-env.age".publicKeys = [mihai germanium];
 }
