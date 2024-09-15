8afc214582c240fb09661d292691e310a084b308
Mihai Fufezan <mihai@fufexan.net>
2024-09-16 02:26:27 +0300

    modules: remove unused modules

--- modules/default.nix
+++ modules/default.nix
@@ -8,9 +8,6 @@
       _module.args.shared_modules = [
         {_module.args = {inherit inputs self;};}
         inputs.agenix.nixosModules.default
-        inputs.disko.nixosModules.disko
-        inputs.impermanence.nixosModules.impermanence
-
         inputs.srvos.nixosModules.server
         ./core.nix
       ];
