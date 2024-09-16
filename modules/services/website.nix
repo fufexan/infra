5b94661f071345cd0209c94cfe8573f048295182
Mihai Fufezan <mihai@fufexan.net>
2024-09-16 19:34:06 +0300

    website: set up 404 response page

--- modules/services/website.nix
+++ modules/services/website.nix
@@ -3,6 +3,15 @@
     encode zstd gzip
     root * /var/www/fufexan.net
     file_server
+
+    handle_errors {
+      @404 {
+        expression {http.error.status_code} == 404
+      }
+
+      rewrite @404 /404.html
+      file_server
+    }
   '';
 
   services.caddy.virtualHosts."dots.fufexan.net".extraConfig = ''
