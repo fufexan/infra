e12c48657f16e9025564c5457a01df66697a08af
Mihai Fufezan <mihai@fufexan.net>
2024-10-06 12:23:17 +0300

    services/website: cv redirect

--- modules/services/website.nix
+++ modules/services/website.nix
@@ -1,20 +1,26 @@
 {
-  services.caddy.virtualHosts."fufexan.net".extraConfig = ''
-    encode zstd gzip
-    root * /var/www/fufexan.net
-    file_server
-
-    handle_errors {
-      @404 {
-        expression {http.error.status_code} == 404
-      }
-
-      rewrite @404 /404.html
+  services.caddy.virtualHosts = {
+    "fufexan.net".extraConfig = ''
+      encode zstd gzip
+      root * /var/www/fufexan.net
       file_server
-    }
-  '';
 
-  services.caddy.virtualHosts."dots.fufexan.net".extraConfig = ''
-    redir https://github.com/fufexan/dotfiles
-  '';
+      handle_errors {
+        @404 {
+          expression {http.error.status_code} == 404
+        }
+
+        rewrite @404 /404.html
+        file_server
+      }
+    '';
+
+    "cv.fufexan.net".extraConfig = ''
+      redir https://github.com/fufexan/cv
+    '';
+
+    "dots.fufexan.net".extraConfig = ''
+      redir https://github.com/fufexan/dotfiles
+    '';
+  };
 }
