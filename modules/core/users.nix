48a9d0c0e9ee63c893e240aae1b9751580fc46ee
Mihai Fufezan <mihai@fufexan.net>
2024-11-05 08:45:37 +0200

    users: add razvan

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -7,4 +7,12 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOq9Gew1rgfdIyuriJ/Ne0B8FE1s8O/U2ajErVQLUDu9 mihai@io"
     ];
   };
+
+  users.users.razvan = {
+    isNormalUser = true;
+    initialPassword = "123";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXCgRRSDBe37h5zq5Aif6KuRn6HHfpwmmsNTFZvP8vq razvan@germanium"
+    ];
+  };
 }
