271ed9c37b742c7b07173671f4e3b6a75cd87721
Mihai Fufezan <mihai@fufexan.net>
2024-11-16 17:24:45 +0200

    secrets: add mihai-password

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -1,11 +1,21 @@
 {
+  self,
+  config,
+  ...
+}: {
+  age.secrets.mihai-password = {
+    file = "${self}/secrets/mihai-password.age";
+    owner = "root";
+    mode = "400";
+  };
+
   users.users.mihai = {
     isNormalUser = true;
     extraGroups = ["wheel"];
-    initialPassword = "123";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOq9Gew1rgfdIyuriJ/Ne0B8FE1s8O/U2ajErVQLUDu9 mihai@io"
     ];
+    hashedPasswordFile = config.age.secrets.mihai-password.path;
   };
 
   users.users.razvan = {
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,6 +2,10 @@ let
   mihai = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOq9Gew1rgfdIyuriJ/Ne0B8FE1s8O/U2ajErVQLUDu9 mihai@io";
 
   germanium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH59Ji3FQo4DkTW9knLuXpX/BC7IEEfiO65CJdT7Dw2l root@germanium";
+  homesv = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUrerO2XIHlOUZmPly+r7d9puX8dSN6bMyypJ6zVBra root@nixos";
+
+  servers = [germanium homesv];
 in {
   "vaultwarden-env.age".publicKeys = [mihai germanium];
+  "mihai-password.age".publicKeys = [mihai] ++ servers;
 }
