a2cc5588a87b49dfbd32c384b5aa59f81554c554
Mihai Fufezan <mihai@fufexan.net>
2025-10-14 18:53:48 +0300

    services/tailscale: add ethtool perf improvements

--- modules/networking/tailscale.nix
+++ modules/networking/tailscale.nix
@@ -1,4 +1,8 @@
 {
+  lib,
+  pkgs,
+  ...
+}: {
   # used by tailscale for exit node
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = true;
@@ -11,5 +15,18 @@
   services.tailscale = {
     enable = true;
     openFirewall = true;
+  };
+
+  services = {
+    networkd-dispatcher = {
+      enable = true;
+      rules."50-tailscale" = {
+        onState = ["routable"];
+        script = ''
+          NETDEV="$(ip -o route get 8.8.8.8 | cut -f 5 -d ' ')"
+          ${lib.getExe pkgs.ethtool} -K "$NETDEV" rx-udp-gro-forwarding on rx-gro-list off
+        '';
+      };
+    };
   };
 }
