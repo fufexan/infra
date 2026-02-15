803513a5bb0582e4c3a87025a1f1533f22ec809e
Mihai Fufezan <mihai@fufexan.net>
2026-02-15 21:50:00 +0200

    hosts/toshiba: set stateVersion, update logind setting

--- hosts/toshiba/default.nix
+++ hosts/toshiba/default.nix
@@ -13,11 +13,13 @@
   networking.hostName = "toshiba";
 
   # don't suspend when lid is closed
-  services.logind.lidSwitch = "ignore";
+  services.logind.settings.Login.HandleLidSwitch = "ignore";
 
   users.users.user = {
     isNormalUser = true;
     createHome = false;
     hashedPassword = "$y$j9T$Ne9tVneuIjJslba3ErU0Z.$peEWu/MZQ1sZwkqY2vMK8eGDBCJqJt2T0uvmqLKnDP0";
   };
+
+  system.stateVersion = "26.05";
 }
