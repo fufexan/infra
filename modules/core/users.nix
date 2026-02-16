73be8c0e5b966ef9d8cdbc383a4d1e3fa7644207
Mihai Fufezan <mihai@fufexan.net>
2026-02-16 15:15:54 +0200

    Revert "users: drop hashed pass temporarily"
    This reverts commit 2568de88eda270b74c3e10d211c5174ccad132ba.

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -1,4 +1,8 @@
-{self, ...}: {
+{
+  self,
+  config,
+  ...
+}: {
   age.secrets.mihai-password = {
     file = "${self}/secrets/mihai-password.age";
     owner = "root";
@@ -11,6 +15,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOq9Gew1rgfdIyuriJ/Ne0B8FE1s8O/U2ajErVQLUDu9 mihai@io"
     ];
+    hashedPasswordFile = config.age.secrets.mihai-password.path;
   };
 
   users.users.razvan = {
