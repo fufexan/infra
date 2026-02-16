5099011f820093f73a1c829610a9525fd88bd296
Mihai Fufezan <mihai@fufexan.net>
2026-02-16 15:15:51 +0200

    secrets: add toshiba

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,8 +3,9 @@ let
 
   germanium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH59Ji3FQo4DkTW9knLuXpX/BC7IEEfiO65CJdT7Dw2l root@germanium";
   homesv = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUrerO2XIHlOUZmPly+r7d9puX8dSN6bMyypJ6zVBra root@nixos";
+  toshiba = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOJuGwyt3x9arBCTvoh22zBAyK+SSXYOLs68d0Ti13qy root@homesv";
 
-  servers = [germanium homesv];
+  servers = [germanium homesv toshiba];
 in {
   "vaultwarden-env.age".publicKeys = [mihai germanium];
   "mihai-password.age".publicKeys = [mihai] ++ servers;
